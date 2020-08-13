f987e1773a347a1ea655984f630848f881b92b46
zinyawaihtet <zyawai99@gmail.com>
2020-08-13 12:59:23 +0630

    update

--- resources/views/backend/items/show.blade.php
+++ resources/views/backend/items/show.blade.php
@@ -3,39 +3,23 @@
 @section('content')
 
 <div class="container-fluid">
-	<h2>Item Detail (Form) </h2>
+	<h2 class="text-gray-800 py-3">Item Detail (Form) </h2>
 	<div class="row">
 	
-	<div class=" row col-md-8 mt-2 pt-2">
-	<table class="table table-bordered">
-		<thead>
-			<tr>
-				
-				<th>Photo</th>
-				<th>Name</th>
-				<th>Code</th>
-				<th>Price</th>
-				
-
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><img src="{{asset($item->photo)}}"></td>
-				<td>{{$item->name}}</td>
-				<td>{{$item->codeno}} <a href="{{route('items.show',$item->id)}}"></a></td>
-				
-				<td>{{$item->price}} MMK</td>
-				
-				
-			</tr>
-			
-		</tbody>
-		
-	</table>
-
+	<div class="col-md-4 px-5">
+		<img src="{{asset($item->photo)}}" class="img-fluid">
 	</div>
+	
+ 		<div class="col-md-8 text-justify">
+
+			<h5 class="text-dark">Name: <span class="text-muted">{{$item->name}}</span></h5>
+			<h5 class="text-dark">Code Number: <span class="text-muted">{{$item->codeno}} <a href="{{route('items.show',$item->id)}}"></a></span></h5>
+				
+			<h5 class="text-dark">Price: <span class="text-muted">{{$item->price}} MMK</span></h5>
+
+		</div>
 	</div>
+	
 </div>
 
 @endsection

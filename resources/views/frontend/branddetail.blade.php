f91fbd8e7bd65144202f804f44a3268a84c8683f
zinyawaihtet <zyawai99@gmail.com>
2020-08-14 16:26:08 +0630

    vaildation, login,register,authentication,pivot

--- resources/views/frontend/branddetail.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('frontendtemplate')
-@section('content')
-<div class="col-lg-6">
-	<h2>Brand Detail Page</h2>
-	
-</div>
-@endsection
--- resources/views/frontend/brands.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('frontendtemplate')
-@section('content')
-<div class="col-lg-6">
-	<h2>Brands Page</h2>
-	
-</div>
-@endsection
